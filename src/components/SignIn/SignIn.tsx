38117da15c2a77faebfa753c2ce918f7b339627a
Tyler Kim <tylerkim.nm@gmail.com>
2022-11-28 14:42:40 -0800

    first commit

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign in</h1>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Dashboard, SignIn } from './components';
 import reportWebVitals from './reportWebVitals';
+import './styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home title = {'Car Collection'}/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path='signin' element={<SignIn/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
